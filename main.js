2a8e1243b8dc7d2fbfd2784537dff542df275008
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-24 10:40:45 +0100

    Add move background color

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ const h1 = document.querySelector("h1");
 document.body.addEventListener('mousemove', (e) => {
   console.log(e.clientX, e.clientY);
   h1.textContent = `${e.clientX}, ${e.clientY}`;
+
+  document.body.style.backgroundColor = `rgb(${e.clientX / 3}, ${e.clientY / 2}, ${e.clientX / e.clientY * 20})`;
 })
+
